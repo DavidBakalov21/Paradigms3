12a41facb57f3a25487a688181bba56b0ab981c5
DavidBakalov21 <davidbakalov33@gmail.com>
2023-10-04 18:56:40 +0300

    first steps

---
+++ Paradigms3/Paradigms3.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <iostream>
+
+
+
+std::string Encrypt(std::string str, int key) {
+
+    std::string encrypted = str;
+
+
+    for (int i = 0; i < encrypted.size(); i++)
+    {
+        if (std::isalpha(encrypted[i])) {
+            char base = 'a';
+            if (std::isupper(encrypted[i]) == true) {
+                base = 'A';
+            }
+            encrypted[i] = static_cast<char>(base + (encrypted[i] - base + key) % 26);
+        }
+    }
+
+
+    return encrypted;
+}
+
+
+int main()
+{
+    std::cout << Encrypt("Hsello, World!", 3);
+
+}
+
