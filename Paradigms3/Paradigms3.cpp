a2c63e57322bb7b4e1f23c10b3f97a7869b35cbc
DavidBakalov21 <davidbakalov33@gmail.com>
2023-10-04 19:40:00 +0300

    decrypt, encrypt

--- Paradigms3/Paradigms3.cpp
+++ Paradigms3/Paradigms3.cpp
@@ -23,10 +23,29 @@ std::string Encrypt(std::string str, int key) {
     return encrypted;
 }
 
+std::string Decrypt(std::string str, int key) {
+    std::string decrypted = str;
+
+    for (int i = 0; i < decrypted.size(); i++)
+    {
+        if (std::isalpha(decrypted[i])) {
+            char base = 'a';
+            if (std::isupper(decrypted[i]) == true) {
+                base = 'A';
+            }
+            decrypted[i] = static_cast<char>(base + (decrypted[i] - base - key + 26) % 26);
+        }
+    }
+
+
+    return decrypted;
+}
+
 
 int main()
 {
-    std::cout << Encrypt("Hsello, World!", 3);
-
+    
+    std::cout << Encrypt("Hello, World!", 3);
+    std::cout << Decrypt("Khoor, Zruog!", 3);
 }
 
